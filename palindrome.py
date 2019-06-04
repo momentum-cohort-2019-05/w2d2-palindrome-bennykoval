405766b2a3668ab6e3dd5eec65a88cdefc74dc99
bennykoval <bethanykoval@gmail.com>
2019-06-04 11:53:45 -0400

    Add function to return user sentence backwards

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,21 @@
 user_sentence = input("Enter a sentence or two: ")
+all_letters = "abcdefghijklmnopqrstuvwxyz"
+found_letters = []
 
-if user_sentence is user_sentence[::-1]:
-    print("is a palindrome")
-else:
-    print("is not a palindrome")
+def return_backwards(user_sentence): 
+  return user_sentence[::-1]
+
+print(user_sentence)
+
+#for letter in user_sentence.lower():
+    #if letter in all_letters and letter not in found_letters:
+        #found_letters.append(letter)
+
+#if found_letters =
+#if user_sentence is user_sentence[::-1]:
+    #print("is a palindrome")
+#else:
+    #print("is not a palindrome")
+        
+#print(found_letters)
+#print(sorted(found_letters))
